08e33f875af532cce387d9cb2f5a6d19e0ef7cc7
Vancesca Dinh <dinh.vancesca@gmail.com>
2025-09-30 22:03:43 +0200

    query embedding and search tool

---
+++ search_tools.py
@@ -0,0 +1,23 @@
+from typing import List, Any
+from sentence_transformers import SentenceTransformer
+
+embedding_model = SentenceTransformer('multi-qa-distilbert-cos-v1')
+
+
+
+class SearchTool:
+    def __init__(self, index):
+        self.index=index
+
+    def search(self, query: str) -> List[Any]:
+        """
+        Perform a text-based search on the FAQ index.
+
+        Args:
+            query (str): The search query string.
+
+        Returns:
+            List[Any]: A list of up to 5 search results returned by the FAQ index.
+        """
+        query_embedding = embedding_model.encode(query)
+        return self.index.search(query_embedding, num_results=5)
