b9a61a2f2d9e09ba920f3ad8f2b5c7d75702bbd6
Vancesca Dinh <dinh.vancesca@gmail.com>
2025-10-01 10:22:07 +0200

    minor typo

--- search_tools.py
+++ search_tools.py
@@ -11,13 +11,13 @@ class SearchTool:
 
     def search(self, query: str) -> List[Any]:
         """
-        Perform a text-based search on the FAQ index.
+        Perform a text-based search on the Elastic Search index.
 
         Args:
             query (str): The search query string.
 
         Returns:
-            List[Any]: A list of up to 5 search results returned by the FAQ index.
+            List[Any]: A list of up to 5 search results returned by the Elastic Search index.
         """
         query_embedding = embedding_model.encode(query)
         return self.index.search(query_embedding, num_results=5)
